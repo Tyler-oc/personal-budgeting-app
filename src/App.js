945e6071f5ac9504803d828b10b6a976ac414ecb
Tyler Onstad-Cran <tonstadcran@gmail.com>
2023-11-05 15:16:02 -0700

    budget modal and cards working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import { Container, Stack } from 'react-bootstrap';
-import { Button } from 'bootstrap';
+import { Container, Stack, Button } from 'react-bootstrap';
 import BudgetCard from './components/BudgetCard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App_style.css';
+import AddBudgetModal from './components/AddBudgetModal';
+import { useState } from 'react';
+import { useBudgets } from './contexts/BudgetsContext';
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
+  const { budgets, getBudgetExpenses } = useBudgets()
+
   return (
     <Container className='my-4'>
       <Stack direction="horizontal" gap="2" className='mb-4'>
         <h1 className='me-auto'>Budget</h1>
-        <Button variant="primary">Add Budget</Button>
+        <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
         <Button variant="outline-primary">Add Expense</Button>
       </Stack>
       <div id='budget-card-div'>
-        <BudgetCard></BudgetCard>
+        {budgets.map(budget => {
+          const amount = getBudgetExpenses(budget.id).reduce((total, expense) => total + expense.amount, 0)
+          return (
+            <BudgetCard 
+              key={budget.id}
+              name={budget.name}
+              amount={amount}
+              max={budget.max}
+            />
+          )
+        })}
       </div>
+      <AddBudgetModal show={showAddBudgetModal} handleClose={() => setShowAddBudgetModal(false)} />
     </Container>
-  );
+  )
 }
 
 export default App;
